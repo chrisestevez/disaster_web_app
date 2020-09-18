9b8314323398d37c2f4877f85ef8a27b99cbd8eb
chrisestevez <chrisestevez1@gmail.com>
2020-09-18 00:06:55 -0400

    feat: ML pipeline
    - Created function to evaluate model
    
    - Created function to save model after training
    
    - Documented project

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -7,8 +7,6 @@ nltk.download([ 'stopwords', 'punkt', 'wordnet','averaged_perceptron_tagger'])
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem.porter import PorterStemmer
-
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -16,47 +14,48 @@ from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report,accuracy_score
+import pickle
 from sklearn.base import BaseEstimator, TransformerMixin
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP']:
-                return True
-        return False
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-
 def load_data(database_filepath):
+    """Loads data from database.
+
+    Args:
+        database_filepath (str): Path to database
+
+    Returns:
+        obj: Returns X, Y, and category_names
+    """
     
     table ='disaster_data'
     
-    engine = sqlalchemy.create_engine("sqlite:///%s" % database_filepath)
+    engine = sqlalchemy.create_engine("sqlite:///"+database_filepath) 
     
     df = pd.read_sql_table(table, engine)
+
     X = df['message']
+    
     Y = df.drop(columns=['original', 'genre','id','message'])
+    
     category_names = Y.columns
     
-    return X, Y, category_names # df, 
-
+    return X, Y, category_names
 
 def tokenize(text):
+    """normalizes,lemmatize, and tokenizes text.
+
+    Args:
+        text (str): Text to be normalized.
+
+    Returns:
+        obj: Returns list of tokens.
+    """    
     
     stop_words = stopwords.words("english")
+    
     lemmatizer = WordNetLemmatizer()
-    
-    
+        
     # normalize case and remove punctuation
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
      
@@ -68,42 +67,41 @@ def tokenize(text):
     
     # Lemmatize verbs by specifying pos
     tokens = [WordNetLemmatizer().lemmatize(w, pos='v') for w in tokens]
-    
-    # Reduce words to their stems
-    tokens = [PorterStemmer().stem(w) for w in tokens]
-    
+        
     return tokens
 
+def build_model(clf=RandomForestClassifier(random_state=143)):
+    """Builds model using sklearn pipeline and GridSearchCV.
 
-def build_model(clf=RandomForestClassifier(random_state=143, n_jobs=-2)):
-    
+    Args:
+        clf (obj, optional): Classifier used. Defaults to \
+            RandomForestClassifier(random_state=143).
 
-    
-    
+    Returns:
+        obj: Model.
+    """
+  
     pipeline = Pipeline([
         ('features', FeatureUnion([
 
             ('text_pipeline', Pipeline([
                 ('vect', CountVectorizer(tokenizer=tokenize)),
                 ('tfidf', TfidfTransformer())
-            ]))#,
-
-            #('starting_verb', StartingVerbExtractor())
+            ]))
         ])),
         ('clf', MultiOutputClassifier(clf))])
 
     parameters = {
     # 'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
     # 'features__text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
-    # 'features__text_pipeline__vect__max_features': (None,500, 1000, 2500, 5000,
-    #                                                  10000),
-    # 'features__text_pipeline__tfidf__use_idf': (True, False)#,
-    'clf__estimator__bootstrap': [True, False]#,
-    # 'clf__estimator__max_depth': [10, 25, 50, None],
+    # 'features__text_pipeline__vect__max_features': (500, 2500, 5000, 10000),
+    # 'features__text_pipeline__tfidf__use_idf': (True, False),
+    # 'clf__estimator__bootstrap': [True, False],
+    # 'clf__estimator__max_depth': [25, 50, 100, 250, 500], 
     # 'clf__estimator__max_features': ['auto', 'sqrt'],
-    # 'clf__estimator__min_samples_leaf': [1, 2, 4],
-    # 'clf__estimator__min_samples_split': [2, 5, 10],
-    # 'clf__estimator__n_estimators': [50, 100, 250, 500, 1000],
+    # 'clf__estimator__min_samples_leaf': [1, 2],
+    # 'clf__estimator__min_samples_split': [2],
+    'clf__estimator__n_estimators': [25]
     # 'clf__estimator__criterion': ['entropy', 'gini'],
     # 'features__transformer_weights': (
     #         {'text_pipeline': 1, 'starting_verb': 0.5},
@@ -112,22 +110,41 @@ def build_model(clf=RandomForestClassifier(random_state=143, n_jobs=-2)):
                 
                  }
 
-    cv = GridSearchCV(pipeline, param_grid=parameters, cv = 3) #  n_jobs=-2,
-
+    cv = GridSearchCV(pipeline, param_grid=parameters, cv=2,verbose=1)
+ 
     return cv
 
-
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """Evaluates fitted model using various metrics.
 
+    Args:
+    
+        `model` (obj): Fitted model.
+        
+        `X_test` (obj): X testing data.
+        
+        `Y_test` (obj): Y testing data.
+        
+        `category_names` (obj): Names of classification columns.
+    """
+    Y_pred = model.predict(X_test)
+    
+    print(classification_report(Y_test,Y_pred, target_names=category_names))
+    
+    print(f' Accuracy {round(accuracy_score(Y_test, Y_pred)*100,2)}','%')
 
-def save_model(model, model_filepath):
-    pass
+def save_model(model):
+    """Saves trained model to a pickle file.
 
+    Args:
+        model (obj): Trained model.
+    """
+    pickle.dump(model, open('classifier.pkl', 'wb'))
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
+    print(sys.argv)
+    if len(sys.argv) == 2:
+        database_filepath = sys.argv[1]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y,\
@@ -142,37 +159,20 @@ def main():
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        print('Saving model...\n    MODEL: classifier.pkl')
+        save_model(model)
 
         print('Trained model saved!')
 
     else:
         print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
-
+              'as the argument'\
+              ' \n\nExample: python'\
+              'train_classifier.py ../data/DisasterResponse.db')
 
 if __name__ == '__main__':
-    
-    print('-'*50)
-    
-    database_filepath ='./data/DisasterResponse.db'
-    
-    X, Y, category_names = load_data(database_filepath)
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y,\
-             test_size=0.2, random_state=143 )
-    
-    print('-'*50)
-    print('Building model...')
-    model = build_model()
-    
-    print('-'*50)    
-    print('Training model...')
-    model.fit(X_train, Y_train)
-  
  
+    main()
     
    
     
